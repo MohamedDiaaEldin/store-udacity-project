8e104c63b31a18c51c5125bcf67571cba6b17ebd
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-05-11 22:08:42 +0200

    update local ip

--- run_database.sh
+++ run_database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-sudo  docker run -d -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=mohamed -e POSTGRES_DB=store -p 192.168.41.12:5432:5432 -v /home/mohamed/Desktop/udacity/advanced-web-udacity_v1/second/proj/data:/var/lib/postgresql/data postgres
+sudo  docker run -d -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=mohamed -e POSTGRES_DB=store -p 192.168.1.100:5432:5432 -v /home/mohamed/Desktop/udacity/advanced-web-udacity_v1/second/proj/data:/var/lib/postgresql/data postgres
                                                                                                       
