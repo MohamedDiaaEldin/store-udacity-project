0ae67d2d0b10bd61e865474ec9189762a3e67fac
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-05-17 11:23:06 +0200

    remove dev bash

--- run_database.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-sudo  docker run -d -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=mohamed -e POSTGRES_DB=store --net="host" -v /home/mohamed/Desktop/udacity/advanced-web-udacity_v1/second/proj/data:/var/lib/postgresql/data postgres
-                                                                                                      
--- run_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-sudo docker run --net="host" -v /home/mohamed/Desktop/udacity/advanced-web-udacity_v1:/app -it ubuntu:node bash
